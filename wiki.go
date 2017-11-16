43c58c6801d84363a13e6af72b306bbb83321b3c
Nick Holland <nkh@bluetel.co.uk>
2017-11-16 15:12:10 +0000

    added Page struct and saving to txt file

--- wiki.go
+++ wiki.go
@@ -4,3 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 )
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
