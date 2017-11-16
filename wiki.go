b8a6df592abb5d67eb2762a596c9b0e00479d6fd
Nick Holland <nkh@bluetel.co.uk>
2017-11-16 20:01:52 +0000

    added load page method

--- wiki.go
+++ wiki.go
@@ -14,3 +14,12 @@ func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
+
+func loadPage(title Page) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, error
+	}
+	return &Page{Title: title, Body: body}, nil
+}
