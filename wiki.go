94d443ad13849f9822c2357d58ddd791639e8867
Nick Holland <nkh@bluetel.co.uk>
2017-11-16 20:20:28 +0000

    resolved type error not expression

--- wiki.go
+++ wiki.go
@@ -19,7 +19,7 @@ func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return nil, error
+		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
 }
