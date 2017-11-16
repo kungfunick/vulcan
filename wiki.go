7e5d430b6f098c65abf702b9d4d7ea0bb6e5d8ce
Nick Holland <nkh@bluetel.co.uk>
2017-11-16 20:17:55 +0000

    removed extra func declaration

--- wiki.go
+++ wiki.go
@@ -15,12 +15,6 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) *Page {
-	filename := title + ".txt"
-	body, _ :=ioutil.ReadFile(filename)
-	return &Page{Title: title, Body: body}
-}
-
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
